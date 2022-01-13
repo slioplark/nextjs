da461ded343591fd660057c24b7f196f5e501c63
Mason <jeailfy9520@outlook.com>
2022-01-13 16:04:15 +0800

    feat: set paths

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
+import styles from '@styles/Home.module.css'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import React from 'react'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
